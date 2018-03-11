919bee69eb02e5aa500c6fc4c5d22ea85c343efa
jorggerojas <jorgelroar@gmail.com>
2018-03-10 19:47:40 -0600

    carpeta php y login con ajax (SIN registro)

---
+++ Make-Up Me/Make-Up Me/php/selectUsuarios.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+	require_once 'conexion.php';
+	$funciones = new Funciones();
+	$conexion = $funciones->conectar();
+	$conexion->set_charset("utf8");
+	$query = "SELECT * FROM usuarios";
+	$resultado = $conexion->query($query);
+	$arreglo = array();
+	while ($r = $resultado->fetch_object()) {
+		array_push($arreglo, array(
+			"idUsuario"=>$r->idUsuario,
+			"nombre"=>$r->Nombre,
+			"apellidos"=>$r->Apellidos,
+			"correo"=>$r->Correo,
+			"pass" =>$r->Password
+		));
+	}
+	
+	
+	//IMPRIMIR LA RESPUESTA EN JSON
+	echo json_encode($arreglo);
+
+ ?>
