788c1125712e29975486e91e3e229834a72ebe8d
jorggerojas <jorgelroar@gmail.com>
2018-03-12 21:15:14 -0500

    Add files via upload

---
+++ BussApp/php/selectHistorial.php
@@ -0,0 +1,22 @@
+<?php 
+
+	$funciones = new Funciones();
+	$conexion = $funciones->conectar();
+	$conexion->set_charset("utf8");
+
+	$query = "SELECT * FROM historial;";
+	$resultado = $conexion->query($query);
+	$arreglo = array();
+	while ($r = $resultado->fetch_object()) {
+		array_push($arreglo, array(
+			"idHistoria"=>$r->idHistoria,
+			"ruta"=>$r->ruta,
+			"importe"=>$r->importe,
+			"fecha"=>$r->fecha,
+			"correo"=>$r->correo
+		));
+	}
+
+	echo json_encode($arreglo);
+
+ ?>
