c17c7854f85a67cb18a62ada378672f22952bb05
jorggerojas <jorgelroar@gmail.com>
2018-03-27 00:23:20 -0500

    Make Up acabado sin bootstrap

---
+++ Make-Up Me Sin Bootstrap/php/conexion.php
@@ -0,0 +1,26 @@
+<?php 
+class Funciones {
+	public function conectar() {
+		$host = "localhost";
+		$user = "root";
+		$pass = "";
+		$db = "makeUpMe";
+
+		$conexion = new mysqli($host, $user, $pass, $db);
+
+
+
+		if ($conexion-> connect_errno > 0) {
+			echo "ERROR:".$conexion->error."<br>";
+		} else {
+			
+			return $conexion;
+		}
+	}
+
+	public function desconectar($conexion){
+		mysql_close($conexion);
+	}
+}
+
+ ?>
