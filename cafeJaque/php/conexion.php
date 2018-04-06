2eb08a77454d16aaba9d35feb780fcddebecdf28
Unknown <torrewz@gmail.com>
2018-04-06 11:43:22 -0500

    Conexion remota a servidor SnackApp

--- cafeJaque/php/conexion.php
+++ cafeJaque/php/conexion.php
@@ -3,9 +3,9 @@
 class Funciones {
 	public function conectar() {
 		$host = "localhost";
-		$user = "root";
-		$pass = "";
-		$db = "snackapp";
+		$user = "id5309171_root";
+		$pass = "12345";
+		$db = "id5309171_snackapp";
 
 		$conexion = new mysqli($host, $user, $pass, $db);
 
