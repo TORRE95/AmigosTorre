7a301450fb6fa96a6c4036f12918d93a9bb34f51
xZkan <frincon20@alumnos.uaq.mx>
2018-04-05 22:43:33 -0500

    nueva BD, Las cafeterias se jalan por ajax y los productos de cada cafe igual

--- cafeJaque/cafejaque.sql
+++ cafeJaque/cafejaque.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-04-2018 a las 02:21:32
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 5.6.30
+-- Tiempo de generación: 06-04-2018 a las 05:41:04
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -76,7 +78,8 @@ CREATE TABLE `productos` (
 INSERT INTO `productos` (`idProducto`, `Nombre`, `Descripcion`, `Precio`, `idUsuario`) VALUES
 (2, 'Jabon', '12kjbn,', '123.00', 0),
 (3, 'Jabon', 'Jabon', '558.00', 0),
-(4, 'carne', 'asada', '455.00', 0);
+(4, 'carne', 'asada', '455.00', 0),
+(5, 'Caca', 'Pedacito de popo', '15.00', 2);
 
 -- --------------------------------------------------------
 
@@ -93,18 +96,20 @@ CREATE TABLE `usuarios` (
   `Password` varchar(500) NOT NULL,
   `nombreUsuario` varchar(50) NOT NULL DEFAULT '0',
   `Telefono` varchar(50) DEFAULT '0',
-  `FechaRegistro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `FechaRegistro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `DescripcionCafe` varchar(50) DEFAULT NULL,
+  `Rate` varchar(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`idUsuario`, `tipoUsuario`, `Nombre`, `Apellidos`, `Correo`, `Password`, `nombreUsuario`, `Telefono`, `FechaRegistro`) VALUES
-(1, 0, 'Jorge', '', 'jorge@mail.com', '12345', '0', '0', '2018-04-04 22:23:10'),
-(2, 1, 'pruebaAdmin', 'Admin', 'admin@mail.com', '12345', '0', '0', '2018-04-04 20:48:40'),
-(6, 0, 'pruebaUser', 'usuarioNormal', 'mail@mail.com', '12345', '0', '0', '2018-04-04 20:48:14'),
-(7, 0, 'Luis', 'Arcos', 'luis@mail.com', '12345', 'soy jorge', '4423311744', '2018-04-05 23:04:34');
+INSERT INTO `usuarios` (`idUsuario`, `tipoUsuario`, `Nombre`, `Apellidos`, `Correo`, `Password`, `nombreUsuario`, `Telefono`, `FechaRegistro`, `DescripcionCafe`, `Rate`) VALUES
+(1, 0, 'Jorge', '', 'jorge@mail.com', '12345', 'JORGE', '4423698083', '2018-04-04 22:23:10', NULL, NULL),
+(2, 1, 'pruebaAdmin', 'Admin', 'admin@mail.com', '12345', '0', '0', '2018-04-04 20:48:40', 'Se venden jochos', '4.9'),
+(6, 0, 'pruebaUser', 'usuarioNormal', 'mail@mail.com', '12345', '0', '0', '2018-04-04 20:48:14', NULL, NULL),
+(7, 0, 'Luis', 'Arcos', 'luis@mail.com', '12345', 'soy jorge', '4423311744', '2018-04-05 23:04:34', NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -137,12 +142,15 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `idProducto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idProducto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `idUsuario` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
